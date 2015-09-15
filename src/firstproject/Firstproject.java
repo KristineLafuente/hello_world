a8a02ef8a1dad451c07f42da468959a98e32cf5a
Kristine Lafuente <klafuente1912@stu.neisd.net>
2015-09-15 07:45:12 -0500

    this is my first time on netbeans.

---
+++ src/firstproject/Firstproject.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstproject;
+
+/**
+ *
+ * @author Kristine
+ */
+public class Firstproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ for(int i = 10; i > 0; i--){ 
+     System.out.println("hello, fourth period!"); 
+   }
+ 
+   }
+}
+
+    
